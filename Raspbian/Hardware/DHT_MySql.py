58bcb8812db9b75623954a10ff08d612e03d2491
gyodonggoo <rnryehd111@naver.com>
2020-05-27 15:14:54 +0900

    PythonMysql

--- Raspbian/Hardware/DHT_MySql.py
+++ Raspbian/Hardware/DHT_MySql.py
@@ -13,17 +13,14 @@ conn = pymysql.connect(host='210.119.12.52',user='test_usr',password='mysql_p@ss
 try:
     while True: # loop
         wtime = datetime.datetime.now()
-        humid, temp = dht.read_retry(sensor, pin)
-        
+        humid, temp = dht.read_retry(sensor, pin)       
         if humid is not None and temp is not None:
             print(wtime, 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp,humid))
-
             curs = conn.cursor()
             query = "INSERT INTO shopdb.iotmachine (currents_time, machine_id, temperature, humidity) VALUES(%s, %s, %s, %s)"
             data = (wtime.strftime("%Y-%m-%d %H:%M:%S"),'GGD', temp, humid)
             curs.execute(query, data)
             conn.commit()
-
             time.sleep(10)
         else:
             print('Failed to get readming. Try again!')
